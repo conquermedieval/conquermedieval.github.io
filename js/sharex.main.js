1d9d7f594ed5005ed9afb9089e8c0f39d7b3b15b
cmedieval <152734047+cmedieval@users.noreply.github.com>
2023-12-27 07:53:06 -0300

    Update sharex.main.js

--- js/sharex.main.js
+++ js/sharex.main.js
@@ -31,28 +31,3 @@ function UpdateDownloadButton(release, assetExtension, element) {
         trigger: "hover"
     });
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Data de início do projeto (no formato AAAA-MM-DD)
-    var startDate = new Date('2023-12-23');
-
-    // Função para calcular a diferença em dias
-    function calculateDays() {
-        var currentDate = new Date();
-        var timeDiff = currentDate - startDate;
-        var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-        return daysDiff;
-    }
-
-    // Atualizar o contador
-    function updateCounter() {
-        var counterElement = document.getElementById('counter');
-        counterElement.textContent = 'Dias desde o início do projeto: ' + calculateDays();
-    }
-
-    // Atualizar o contador ao carregar a página
-    updateCounter();
-
-    // Atualizar o contador a cada segundo
-    setInterval(updateCounter, 1000);
-});
