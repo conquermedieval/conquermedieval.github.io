81f02af0b694fd40805710b8362f4f301d323987
cmedieval <152734047+cmedieval@users.noreply.github.com>
2023-12-27 08:02:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Atualizar o contador
     function updateCounter() {
         var counterElement = document.getElementById('counter');
-        counterElement.textContent = 'Dias Online: ' + calculateDays();
+        counterElement.textContent = 'Online ha:  ' + calculateDays();
     }
 
     // Atualizar o contador ao carregar a página
