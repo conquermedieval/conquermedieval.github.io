9351f0a73b79b3c9be1cbbfa31e6e41557bc1350
Conquermedieval <31598257+conquermedieval@users.noreply.github.com>
2025-03-01 19:51:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Atualizar o contador
     function updateCounter() {
         var counterElement = document.getElementById('counter');
-        counterElement.textContent = `🚀Online há: ${calculateDays()} dias 🟢`;
+        counterElement.textContent = `🟢Online há: ${calculateDays()} dias🚀`;
     }
 
     // Atualizar o contador ao carregar a página
