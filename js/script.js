21cf63c3156bc0b46bc904ca992ed753d5131c13
CONQUERMEDIEVAL <31598257+conquermedieval@users.noreply.github.com>
2026-03-24 08:15:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Data de início do projeto (no formato AAAA-MM-DD)
-    var startDate = new Date('2025-02-17');
+    var startDate = new Date('2026-03-23');
 
     // Função para calcular a diferença em dias
     function calculateDays() {
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Atualizar o contador
     function updateCounter() {
         var counterElement = document.getElementById('counter');
-        counterElement.textContent = `🟢Online há: ${calculateDays()} dias🚀`;
+        counterElement.textContent = `🟢 Online há: ${calculateDays()} dias🚀`;
     }
 
     // Atualizar o contador ao carregar a página
